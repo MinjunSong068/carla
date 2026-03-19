b85a05285d9b374c2fe44f9b5cb0acfee6e28b7d
Minjun Song <minjunsong9404@gmail.com>
2026-03-19 14:42:48 -0700

    remove core minimal

--- LibCarla/source/carla/multigpu/primaryCommands.cpp
+++ LibCarla/source/carla/multigpu/primaryCommands.cpp
@@ -4,9 +4,6 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#include "CoreMinimal.h"
-
-
 #include "carla/multigpu/primaryCommands.h"
 
 
