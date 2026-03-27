491d90d88cfcdd9f8e86d9d122eb6dfa3791ea8c
Minjun Song <minjunsong9404@gmail.com>
2026-03-27 01:05:06 -0700

    typo

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServer.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServer.cpp
@@ -836,7 +836,6 @@ void FCarlaServer::FPimpl::BindActions()
 
       //access actor variations, look for "role_name", via the set_attribute function on the python client end, route ID can be appended to the end and worked with
       //access this via GetActorInfo()->Description.Variations in the FCarlaActor class, and look for the "role_name" key
-      Fstring pythonClientData = Episode
 
       return SecondaryServer->GetCommander().GetToken(sensor_id, TCHAR_TO_UTF8(*RoleName)); //send sensor ID and corresponding description 
     }
